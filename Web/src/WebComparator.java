15c388aa21d686f7fb2aa230fc2e64bdaa156ec1
yingting <109306024@nccu.edu.tw>
2022-01-06 10:20:27 +0800

    test

--- Web/src/WebComparator.java
+++ Web/src/WebComparator.java
@@ -1,4 +1,17 @@
 
-public class WebComparator {
+import java.util.Comparator;
 
+public class KeywordComparator implements Comparator<Keyword>{
+	@Override
+	public int compare(Keyword o1, Keyword o2){
+		if(o1==null || o2==null) throw new NullPointerException();
+		//1. compare
+		if(o1.count < o2.count) {
+			return -1;
+		}else if(o1.count > o2.count) {
+			return 1;
+		}
+		return 0;
+	}
+	
 }
--- Web/src/WebHeap.java
+++ Web/src/WebHeap.java
@@ -1,4 +1,43 @@
 
-public class WebHeap {
+import java.util.PriorityQueue;
 
+public class KeywordHeap {
+	private PriorityQueue<Keyword> heap;
+	
+	public KeywordHeap(){
+		this.heap = new PriorityQueue<Keyword>(10, new KeywordComparator());
+	}
+	
+	public void add(Keyword k){
+		heap.offer(k);
+	}
+	
+	public void peek(){
+		if(heap.peek() == null){
+			System.out.println("InvalidOperation");
+			return;
+		}
+		
+		Keyword k = heap.peek();		
+		System.out.println(k.toString());
+	}
+	
+	public void removeMin(){
+		//TODO: write remove minimal element logic here...
+		
+		System.out.println(heap.poll());		
+	}
+	
+	public void output(){
+		//TODO: write output and remove all element logic here...
+		StringBuilder sb = new StringBuilder();
+		Keyword k;
+		
+		while((k = heap.poll()) != null){
+			sb.append(k.toString());
+			if(heap.peek() != null) sb.append("");
+		}
+		
+		System.out.println(sb.toString());	
+	}
 }
